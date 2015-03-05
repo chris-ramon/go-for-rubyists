f1489987eee69ce17e68dce631cb4c2bba9d9eac
Chris <rchristian.ramon@gmail.com>
2015-03-05 17:21:39 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	foo := "foo value"
+	var fooN int
+	fooN = 321
+	fmt.Print(foo)
+	fmt.Print(fooN)
+}
---
+++ main.rb
@@ -0,0 +1,28 @@
+# Types, Variables
+
+foo = "foo value"
+foo = 321
+foo = 321.12
+
+# Arrays
+foo = ["new", "foo", "value"]
+
+# Hashes
+foo = {foo: "foo-val", baz: "baz-val"}
+
+# If/Else
+if true
+  puts "hi"
+end
+
+# Loops
+["hi", "there"].each { |i| puts i }
+
+# functions
+def baz
+  puts "baz called"
+end
+
+# class
+class Bar
+end
